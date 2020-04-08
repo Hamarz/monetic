84a4ddbb016bbe1daa6136208c86be69ec1dd35c
Hamar <Cruel-Dev>
2020-04-08 19:34:09 +0300

    Lisätään pari alku modelia

---
+++ monetic/monetic/Data/Model/Kirjanpitovienti.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace monetic.Data.Model
+{
+    // 1 -123.15 Ruokaostos
+    // 2 -123.15 Pankkitili
+    public class Kirjanpitovienti
+    {
+        public uint Id { get; }
+        public uint Tili { get; set;}
+        public double Summa { get; set; }
+        public string Selite { get; set; }
+
+        public void MuutaTili(uint uusiTili)
+        {
+            Tili = uusiTili;
+        }
+
+        public void MuutaSelite(string uusiSelite)
+        {
+            Selite = uusiSelite;
+        }
+
+        public void MuutaSumma(double uusiSumma)
+        {
+            Summa = uusiSumma;
+        }
+    }
+}
---
+++ monetic/monetic/Data/Model/Tosite.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace monetic.Data.Model
+{
+    public class Tosite
+    {
+        public uint Tositenumero { get; set; }
+        public List<Kirjanpitovienti> Debet { get; set; }
+        public List<Kirjanpitovienti> Kredit { get; set; }
+
+        public bool LisaaVienti(Kirjanpitovienti vienti)
+        {
+            if(VientiLoytyy(vienti))
+                return false;
+
+            if(vienti.Summa < 0)
+                Debet.Add(vienti);
+            else
+                Kredit.Add(vienti);
+
+            return false;
+        }
+
+        public bool PoistaVienti(Kirjanpitovienti vienti)
+        {
+            if(!VientiLoytyy(vienti))
+                return false;
+
+            // Jos se on plussa etsi se debet osiosta
+            if(vienti.Summa > 0)
+                Debet.Remove(vienti);
+            else
+                Kredit.Remove(vienti);
+            return true;
+        }
+
+        public bool VientiLoytyy(Kirjanpitovienti vienti)
+        {
+            return Debet.Find((e) => vienti.Id == e.Id) != null || Kredit.Find((e) => e.Id == vienti.Id) != null;
+        }
+
+        private double debetSumma()
+        {
+            double sum = 0;
+            foreach(var vienti in Debet)
+                sum += vienti.Summa;
+            return sum;
+        }
+
+        private double kreditSumma()
+        {
+            double sum = 0;
+            foreach(var vienti in Kredit)
+                sum += vienti.Summa;
+            return sum;
+        }
+
+        public double Erotus => debetSumma() + kreditSumma();
+    }
+}
